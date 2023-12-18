caa487bf1f73db24697dcca6781854c0dd2a57d4
SilverPlate3 <ariels77@gmail.com>
2023-12-18 08:31:04 +0200

    Coding style alignment

---
+++ task4/task01.c
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * main.c
+ * Eudyptula Challenge: Task 01
+ *
+ * This is a kernel module which prints "Hello World!" to the kernel
+ * debug log when loaded.
+ */
+
+#define MODULE
+#define LINUX
+#define __KERNEL__
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("David Wittman");
+MODULE_DESCRIPTION("Hello World example");
+
+int init_module(void)
+{
+	pr_debug("Hello World!\n");
+	return 0;
+}
+
+
+void cleanup_module(void)
+{
+}
